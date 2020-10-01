c6278c4b58232b314a5dfdf36ca415f3b7c7bda5
HellHound0066 <eddie.mbq@gmail.com>
2020-10-01 13:50:56 +0100

    gf sandwich command

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ client.add_command(lynch)
 #            'botlab': 744145383592296588,
 #            'streak': 745452658545918042}!update
 
-
+@commands.command()
+async def GF_Sandwich(ctx):
+    await ctx.channel.send("UwU what kind of sandwich does daddy want? =^.^=")
 
 async def monthStart():
     while True:
