4c479cf198d598dc1e835779e81cacb4a2a72849
HellHound0066 <eddie.mbq@gmail.com>
2020-10-03 03:52:38 +0100

    Bug fix

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ async def dm(ctx, member: discord.Member, *, content):
 ## Checklist function
 
 @client.command(checks=[is_in_channel3()])
-async def cl(ctx,message):
+async def cl(ctx,message,*):
     channel = client.get_channel(761759598419640341)
     await channel.send(f"<@{ctx.author.id}> - {message}")
 
