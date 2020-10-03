a8e6ee7a9c3180780faee4e12aa6024177690736
HellHound0066 <eddie.mbq@gmail.com>
2020-10-03 15:59:25 +0100

    reverted streaks cog changes

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ from discord.ext import commands
 from discord.utils import get
 import asyncio
 
-#client.add_command(reset)
-#client.add_command(relapse)
-#client.add_command(update)
+client.add_command(reset)
+client.add_command(relapse)
+client.add_command(update)
 #client.add_listener(on_member_ban)
 #client.add_command(clear)
 #client.add_command(kick)
