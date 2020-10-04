b80c5c2811ade39f42a718370aa7e61fe82ab819
HellHound0066 <eddie.mbq@gmail.com>
2020-10-04 12:04:59 +0100

    added a confirmation to the cog load/unload/reload commands

--- main.py
+++ main.py
@@ -32,15 +32,18 @@ async def on_ready():
 @client.command()
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
+    await ctx.send(f'loaded {extension}'')
 
 @client.command()
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
+    await ctx.send(f'unloaded {extension}'')
 
 @client.command()
 async def reload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
     client.load_extension(f'cogs.{extension}')
+    await ctx.send(f'reloaded {extension}'')
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
