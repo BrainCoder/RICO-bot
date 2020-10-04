c0c930d2d2062ea2a8db8dec330b0150aae37542
HellHound0066 <eddie.mbq@gmail.com>
2020-10-04 09:09:07 +0100

    bug fix

--- cogs/checklist.py
+++ cogs/checklist.py
@@ -4,25 +4,23 @@ from discord.utils import get
 
 client=commands.Bot(command_prefix='!')
 
+def is_in_channel3():
+    def inside_fn(ctx):
+        #channelName = 760244981838512158
+        if ctx.channel.id == 761118232161157152:
+            return True
+        return False
+    return inside_fn
 
-
-class Checklist(commands.Cog):
+class checklist(commands.Cog):
 
     def __init__(self, client):
         self.client = client
         self._last_member = None
 
-    def is_in_channel3():
-        def inside_fn(ctx):
-            #channelName = 760244981838512158
-            if ctx.channel.id == 761118232161157152:
-                return True
-            return False
-        return inside_fn
-
     @client.command(checks=[is_in_channel3()])
-    async def cl(ctx,*,message):
-        channel = client.get_channel(761759598419640341)#job board
+    async def cl(self, ctx,*,message):
+        channel = self.client.get_channel(761759598419640341)#job board
         await channel.send(f"<@{ctx.author.id}>: \n{message}")
 
 def setup(client):
