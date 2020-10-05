4bb9153f099e6e61cfd2d7879956f9f410a8d821
HellHound0066 <eddie.mbq@gmail.com>
2020-10-05 19:26:21 +0100

    tidied up main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import asyncio
 client.add_command(reset)
 client.add_command(relapse)
 client.add_command(update)
+
 #Cogs
 @client.command()
 @commands.has_permissions(ban_members=True)
@@ -34,6 +35,7 @@ async def reload(ctx, extension):
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
+#/Cogs
 
 #Member count plus game status
 async def mCount_update():
@@ -47,8 +49,44 @@ async def mCount_update():
     print(f'There are now {mCount} mebers of this server')
     await channel.edit(name=(f'{mCount} members'))
 
+@client.event
+async def on_ready():
+    print('Bot is active')
+    await mCount_update()
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('DM me with complaints!'))
+#/Member count plus game status
 
+#Welcome message
 
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(519455122602983424)
+    await channel.send(f'{member.mention} welcome! Please go to <#519455164894019584> to read an overview of what this server is about. Go to <#519627611836776490> and <#567283111273037834> to see the commands that you can use to assign yourself.')
+
+#/Welcome message
+
+#Complaints DM code
+@client.event
+async def on_message(message):
+    channel = client.get_channel(699110029806272592)
+    if message.guild is None and message.author != client.user:
+        await channel.send(f"<@{message.author.id}> said: {message.content}")
+    await client.process_commands(message)
+
+@client.command(checks=[is_in_channel2()])
+async def dm(ctx, member: discord.Member, *, content):
+    channel = await member.create_dm()
+    await channel.send(content)
+#/Complaints DM code
+
+#Self destruct
+@client.command()
+@commands.has_permissions(administrator=True)
+async def logout(ctx):
+  await ctx.message.delete()
+  await ctx.send("logging out")
+  exit()
+#/Self destruct
 
 """@client.command(pass_context=True)
 @commands.has_permissions(ban_members=True)
@@ -70,18 +108,6 @@ async def endChallenge(ctx):
                     break
     await ctx.send(f"Challenge Winners {len(newParticipants)}")
     print(len(newParticipants))"""
-
-
-@client.event
-async def on_ready():
-    print('Bot is active')
-    await mCount_update()
-    await client.change_presence(status=discord.Status.online, activity=discord.Game('DM me with complaints!'))
-#/Member count plus game status
-
-
-
-
 
 """
 async def monthStart():
@@ -105,11 +131,6 @@ async def hourly():
                 del banDict[key]
 """
 
-
-
-
-
-
 #async def hourly():
 #    while True:
 #        await asyncio.sleep(60*60)
@@ -121,68 +142,10 @@ async def hourly():
 
 
 @client.event
-async def on_member_join(member):
-    channel = client.get_channel(519455122602983424)
-    await channel.send(f'{member.mention} welcome! Please go to <#519455164894019584> to read an overview of what this server is about. Go to <#519627611836776490> and <#567283111273037834> to see the commands that you can use to assign yourself.')
-#/Cogs
-
-#    asyncio.create_task(monthStart())
-  #  asyncio.create_task(hourly())
-
-
-# To ignore command not found and command check exceptions:
-# Use the error cog given in the discord.api support server
-@client.event
 async def on_command_error(ctx, error):
     if isinstance(error, CommandNotFound) or isinstance(error, CheckFailure):
         return
     raise error
-
-
-
-
-
-
-
-## DM Cog
-
-
-#Complaints DM code
-@client.event
-async def on_message(message):
-    channel = client.get_channel(699110029806272592)
-    if message.guild is None and message.author != client.user:
-        await channel.send(f"<@{message.author.id}> said: {message.content}")
-    await client.process_commands(message)
-
-@client.command(checks=[is_in_channel2()])
-async def dm(ctx, member: discord.Member, *, content):
-    channel = await member.create_dm()
-    await channel.send(content)
-#/Complaints DM code
-
-## /DM Cog
-
-## Checklist function
-
-
-
-
-
-#with open ('token.txt', 'rt') as myfile:
- #       contents = myfile.read()
-  #      client.run(f'{contents}')
-#Self destruct
-@client.command()
-@commands.has_permissions(administrator=True)
-async def logout(ctx):
-  await ctx.message.delete()
-  await ctx.send("logging out")
-  exit()
-#Self destruct
-
-
-
 
 #client.run('NzQ5ODM2MjYzOTU1MTAzNzc0.X0xxcA.GYgm0dLg7RX8-8sMUvOTsZtGakc')
 
