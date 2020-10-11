67f25fe0981971d12477dc2341c16ca570e326ad
HellHound0066 <eddie.mbq@gmail.com>
2020-10-11 17:35:49 +0100

    bug fix

--- cogs/MonthlyChallenge.py
+++ cogs/MonthlyChallenge.py
@@ -69,7 +69,7 @@ class MonthlyChallenge(commands.Cog):
     @commands.command()
     @commands.has_any_role('Developer')
     async def participation_amount2(self, ctx):
-        guild = self.ctx.guild
+        guild = ctx.guild
         role = self.guild.get_role(582640858378272793)
         partcipants = [m for m in guild.members if role in m.roles]
         no = len(partcipants)
