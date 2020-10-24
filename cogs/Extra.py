511f9eb01ea488d132e03b7e058b0f868901017d
HellHound0066 <eddie.mbq@gmail.com>
2020-10-24 03:57:23 +0100

    added frame work for Self Mute command

--- cogs/Extra.py
+++ cogs/Extra.py
@@ -1,13 +1,48 @@
 import discord
 from discord.ext import commands
+import sys
+import traceback
+import asyncio
+import re
+import settings
 
 client=commands.Bot(command_prefix='!')
+
+time_regex = re.compile("(?:(\d{1,5})(h|s|m|d))+?")
+time_dict = {"h":3600, "s":1, "m":60, "d":86400}
+
+class TimeConverter(commands.Converter):
+    async def convert(self, ctx, argument):
+        args = argument.lower()
+        matches = re.findall(time_regex, args)
+        time = 0
+        for v, k in matches:
+            try:
+                time += time_dict[k]*float(v)
+            except KeyError:
+                raise commands.BadArgument("{} is an invalid time-key! h/m/s/d are valid!".format(k))
+            except ValueError:
+                raise commands.BadArgument("{} is not a number!".format(v))
 
 class Extra(commands.Cog):
 
     def __init__(self, client):
         self.client = client
         self._last_member = None
+
+    
+    #@commands.command()
+    #@commands.has_any_role('Member')
+    #async def SelfMute(self, ctx, time:TimeConverter = None):
+    #    user = ctx.author
+    #    channel = self.client.get_channel(settings.config["channels"]["log"])
+    #    for discord.guild in self.client.guilds:
+    #        Selfmute_Role = user.guild.get_role(settings.config["statusRoles"]["selfmute"])
+    #    await user.add_roles(Selfmute_Role)
+    #    await channel.send(("Muted {} for {}s" if time else "Muted {}").format(user, time))
+    #    if time:
+    #        await asyncio.sleep(time)
+    #        await user.remove_roles(Selfmute_Role)
 
     @client.command()
     async def DoSomething(self, ctx):
