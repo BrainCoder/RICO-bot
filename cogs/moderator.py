17f44513b00024299d542d8e0aa582e75ead4ba8
Squirrel Dontask <squirrel6946@gmail.com>
2020-11-16 19:34:20 -0600

    Provide revisions for report command

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -6,6 +6,7 @@ import utils
 import settings
 from tabulate import tabulate
 
+
 class ModeratorTools(commands.Cog):
 
     def __init__(self, client):
@@ -16,9 +17,35 @@ class ModeratorTools(commands.Cog):
     @commands.has_any_role(
         settings.config["statusRoles"]["moderator"],
         settings.config["statusRoles"]["semi-moderator"]
-        )
-    async def report(self, ctx, user: discord.User = None, historical = 0):
-        """NPC replacement for the !getstrikes command"""
+    )
+    async def report(self, ctx, user: discord.User = None, historical="historical", output_format="table",
+                     action="mute"):
+        """NPC replacement for the !getstrikes command
+        You can view a report for the most recent moderation actions on any user by just entering the command by itself.
+        You can also choose either historical or current as the third argument to view acting strikes against a user.
+        Format will default to table, but if greater than 2000 characters, it will go to raw output.
+        You can also request that the format be raw, but will require prior arguments to also be filled in.
+        The moderation action it queries against will be mutes(strikes) by default,
+        but it can take the arguments of ban, kick,
+        or will post all moderation actions if any other value is filled in there."""
+        if historical.lower() == "historical":
+            historical = 1
+        elif historical.lower() == "current":
+            historical = 0
+        else:
+            await ctx.send(f'Could not understand whether records should be historical or not. '
+                           f'Please enter either "historical" or "current" as the argument')
+            return
+        mod_action_clause = ""
+        if action == "mute":
+            mod_action_clause = " and me.event_type = 3"
+        elif action == "kick":
+            mod_action_clause = " and me.event_type = 2"
+        elif action == "ban":
+            mod_action_clause = " and me.event_type = 1"
+        if output_format.lower() != "raw" and output_format.lower() != "table":
+            await ctx.send(f'Format option only takes options "raw" or "table".')
+            return
         user_clause = ""
         if user is not None:
             user_clause = f' and recipient_id = {user.id}'
@@ -26,9 +53,38 @@ class ModeratorTools(commands.Cog):
             f'select me.recipient_id, met.mod_action_type, me.event_time, me.issuer_id, me.reason '
             f'from mod_event me '
             f'inner join mod_event_type met on me.event_type = met.mod_type_id where historical = {historical}'
-            f' {user_clause} order by me.event_time desc limit 10')
+            f' {user_clause} {mod_action_clause} order by me.event_time desc limit 20')
         results = utils.conn.execute(prior_mute_queries)
         table = []
+
+        await self.extract_table_from_results(results, table, user)
+        table_output = "```" + tabulate(table,
+                                        headers=["username", "action", "datetime", "moderator", "reason"],
+                                        ) + "```"
+        raw_output = ""
+        if user is None:
+            raw_output += "For various users:\n"
+        else:
+            raw_output = f'For {user}:\n'
+        raw_output = await self.build_raw_output(raw_output, table)
+        if output_format == "table" and len(table_output) < 2000:
+            await ctx.send(table_output)
+        else:
+            if len(raw_output) > 2000:
+                await ctx.send(f'Message exceeds 2,000 characters. Attempting to break into two messages.')
+                midway_point = int(round(len(raw_output) / 2, 0))
+                length = len(raw_output)
+                first_half = raw_output[0:midway_point]
+                last_half = raw_output[midway_point:length]
+                if len(first_half) > 2000 or len(last_half) > 2000:
+                    await ctx.send(f'Output is too long. Please contact a developer to see transcript.')
+                else:
+                    await ctx.send(first_half)
+                    await ctx.send(last_half)
+            else:
+                await ctx.send(raw_output)
+
+    async def extract_table_from_results(self, results, table, user):
         for result in results:
             output = result.values()
             if user is not None and self.client.get_user(user.id) is not None:
@@ -42,16 +98,23 @@ class ModeratorTools(commands.Cog):
             else:
                 name = await self.client.fetch_user(output[3])
                 output[3] = name
-
             table.append(output)
-        await ctx.send("```" + tabulate(table,
-                                        headers=["username", "action", "datetime", "moderator","reason"],
-                                        ) + "```")
+
+    async def build_raw_output(self, raw_output, table):
+        i = 0
+        for row in table:
+            i += 1
+            raw_output += f'{str(i)}. {row[1]} by {row[3]} on {row[2]}'
+            if row[4] is not None:
+                raw_output += f' for reason: {row[4]}'
+            raw_output += "\n"
+        return raw_output
+
     @commands.command()
     @commands.has_any_role(
         settings.config["statusRoles"]["head-moderator"],
         settings.config["statusRoles"]["moderator"],
-        )
+    )
     async def offline(self, ctx, role):
         """Lets you check which users are offline given the name of a role.
         If you want to check for a user with no roles, the phrase is \"empty\".
@@ -91,7 +154,6 @@ class ModeratorTools(commands.Cog):
             for member in members_with_role:
                 nicks += member.display_name + "#" + member.discriminator + "\n"
             await ctx.send(f'Users offline:\n{nicks}')
-
 
 
 def setup(client):
