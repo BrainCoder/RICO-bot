7afbad3c390d5ffebeda8ed0d4b9c9f470bf763e
HellHound0066 <eddie.mbq@gmail.com>
2020-11-29 05:39:02 +0000

    updates version number

--- cogs/background.py
+++ cogs/background.py
@@ -4,7 +4,7 @@ from discord.ext import commands, tasks
 import settings
 from itertools import cycle
 
-status = cycle(['DM me with complaints!', 'Version 1.0.0'])
+status = cycle(['DM me with complaints!', 'Version 1.0.1'])
 
 class background(commands.Cog):
 
