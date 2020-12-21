0112b259ea70f1c93056de2d569c09be108e5cd1
HellHound0066 <eddie.mbq@gmail.com>
2020-12-21 07:49:02 +0000

    tidied up exectuion command

--- cogs/challenge.py
+++ cogs/challenge.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import settings
 import utils
 
-async def challenge(ctx, beforeRole, afterRole):
+async def chalToggle(ctx, beforeRole, afterRole):
     await utils.emoji(ctx, '✅')
     newParticipants = []
     print('Starting new month now.')
@@ -19,11 +19,48 @@ async def challenge(ctx, beforeRole, afterRole):
     await ctx.send(f"Challenge participants {len(newParticipants)}")
     print(len(newParticipants))
 
+async def monthly(ctx, action):
+    if action is None:
+        await ctx.send('Please specify the apropriate arguments for this command')
+    if action == 'start':
+        await chalToggle(ctx, settings.config["challenges"]["monthlyChallengeSignup"], settings.config["challenges"]["monthlyChallengeParticipant"])
+    if action == 'stop':
+        await chalToggle(ctx, settings.config["challenges"]["monthlyChallengeParticipant"], settings.config["challenges"]["monthlyChallengeWinner"])
+
+async def yearly(ctx, action):
+    """Command used to manage the deapool challenge\nStart - Gives everyone w/ the signup role the participant role\nEnd - Gives the finial memeber the winner role"""
+    if action is None:
+        await ctx.send('Please specify the apropriate arguments for this command')
+    if action == 'start':
+        await chalToggle(ctx, settings.config["challenges"]["yearlyChallengeSignup"], settings.config["challenges"]["yearlyChallengeParticipant"])
+    if action == 'stop':
+        await chalToggle(ctx, settings.config["challenges"]["yearlyChallengeParticipant"], settings.config["challenges"]["2021ChallengeWinner"])
+
+async def deadpool(ctx, action):
+    """Command used to manage the deapool challenge\nStart - Gives everyone w/ the signup role the participant role\nEnd - Gives the finial memeber the winner role"""
+    if action is None:
+        await ctx.send('Please specify the apropriate arguments for this command')
+    if action == 'start':
+        await chalToggle(ctx, settings.config["challenges"]["deadpoolSignup"], settings.config["challenges"]["deadpoolParticipant"])
+    if action == 'stop':
+        await chalToggle(ctx, settings.config["challenges"]["deadpoolParticipant"], settings.config["challenges"]["deadpoolWinner"])
+
 class MonthlyChallenge(commands.Cog):
 
     def __init__(self, client):
         self.client = client
         self._last_member = None
+
+    @commands.command(name='challenge', aliases=['chal'])
+    @commands.has_any_role(
+        settings.config["staffRoles"]["developer"])
+    async def challenge(self, ctx, challenge, action):
+        if challenge == 'monthly':
+            await monthly(ctx, action)
+        if challenge == 'yearly':
+            await yearly(ctx, action)
+        if challenge == 'deadpool':
+            await deadpool(ctx, action)
 
     @commands.command(name="participation", pass_context=True)
     async def participation_amount(self, ctx):
@@ -36,47 +73,12 @@ class MonthlyChallenge(commands.Cog):
         dno = len(dparticipants)
         await utils.doembed(ctx, "Challenge statics", "Participation", f"\nMonthly Challenge Memebers left: {mno}\nYearly Challenge Members left: {yno}\nDeadpool Challenge Members Left: {dno}", ctx.author, True)
 
-    @commands.command(name='monthly')
-    @commands.has_any_role(
-        settings.config["staffRoles"]["developer"])
-    async def monthly(self, ctx, action=None):
-        if action is None:
-            await ctx.send('Please specify the apropriate arguments for this command')
-        if action == 'start':
-            await challenge(ctx, settings.config["challenges"]["monthlyChallengeSignup"], settings.config["challenges"]["monthlyChallengeParticipant"])
-        if action == 'end':
-            await challenge(ctx, settings.config["challenges"]["monthlyChallengeParticipant"], settings.config["challenges"]["monthlyChallengeWinner"])
-
     @commands.command(name="yearlychallenge")
     async def yearlychallenge(self, ctx):
         """gives the user the yearly challenge role"""
         signup_role = ctx.guild.get_role(settings.config["challenges"]["yearlyChallengeSignup"])
         await ctx.author.add_roles(signup_role)
         await utils.emoji(ctx, '✅')
-
-    @commands.command(name='yearly')
-    @commands.has_any_role(
-        settings.config["staffRoles"]["developer"])
-    async def yearly(self, ctx, action=None):
-        """Command used to manage the deapool challenge\nStart - Gives everyone w/ the signup role the participant role\nEnd - Gives the finial memeber the winner role"""
-        if action is None:
-            await ctx.send('Please specify the apropriate arguments for this command')
-        if action == 'start':
-            await challenge(ctx, settings.config["challenges"]["yearlyChallengeSignup"], settings.config["challenges"]["yearlyChallengeParticipant"])
-        if action == 'end':
-            await challenge(ctx, settings.config["challenges"]["yearlyChallengeParticipant"], settings.config["challenges"]["2021ChallengeWinner"])
-
-    @commands.command(name='deadpool')
-    @commands.has_any_role(
-        settings.config["staffRoles"]["developer"])
-    async def deadpool(self, ctx, action=None):
-        """Command used to manage the deapool challenge\nStart - Gives everyone w/ the signup role the participant role\nEnd - Gives the finial memeber the winner role"""
-        if action is None:
-            await ctx.send('Please specify the apropriate arguments for this command')
-        if action == 'start':
-            await challenge(ctx, settings.config["challenges"]["deadpoolSignup"], settings.config["challenges"]["deadpoolParticipant"])
-        if action == 'stop':
-            await challenge(ctx, settings.config["challenges"]["deadpoolParticipant"], settings.config["challenges"]["deadpoolWinner"])
 
 
 def setup(client):
