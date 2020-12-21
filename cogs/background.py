10749d5c81793ab9a36c590874cf50919e846ed5
HellHound0066 <eddie.mbq@gmail.com>
2020-12-21 00:41:26 +0000

    1.3.8 - Fixed ids for trial mods in noporn.json

--- cogs/background.py
+++ cogs/background.py
@@ -4,7 +4,7 @@ from discord.ext import commands, tasks
 import settings
 from itertools import cycle
 
-status = cycle(['DM me with complaints!', 'Version 1.3.7', 'Happy holidays from the NoPorn moderation team!'])
+status = cycle(['DM me with complaints!', 'Version 1.3.8', 'Happy holidays from the NoPorn moderation team!'])
 
 class background(commands.Cog):
 
