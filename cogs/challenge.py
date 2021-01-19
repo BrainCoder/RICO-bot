044b581eda92b52bd5e2c67f288d6100d5cf2454
HellHound0066 <eddie.mbq@gmail.com>
2021-01-19 21:37:45 +0000

    1.7.4 - bug fix

--- cogs/challenge.py
+++ cogs/challenge.py
@@ -109,12 +109,7 @@ class MonthlyChallenge(commands.Cog):
         signupRole = ctx.guild.get_role(settings.config["challenges"]["deadpool-signup"])
         await ctx.author.add_roles(signupRole)
         await utils.emoji(ctx)
-
-    @commands.command(name="monthlychallenge")
-    async def monthly_signup(self, ctx):
-        signup_role = ctx.guild.get_role(settings.config["challenges"]["monthly-challenge-signup"])
-        await ctx.author.add_roles(signup_role)
-        await utils.emoji(ctx, '✅')
+        
 
 def setup(client):
     client.add_cog(MonthlyChallenge(client))
