081279b33af1004e4ea2f8ca108fe92ed7484aa0
Eddie <eddie.mbq@protonmail.com>
2021-02-20 12:48:14 +0000

    2.1.5 - Edits to config file

--- cogs/background.py
+++ cogs/background.py
@@ -4,7 +4,7 @@ from discord.ext import commands, tasks
 import settings
 from itertools import cycle
 
-status = cycle(['Version 2.1.4', 'DM me with complaints!'])
+status = cycle(['Version 2.1.5', 'DM me with complaints!'])
 
 class background(commands.Cog):
 
