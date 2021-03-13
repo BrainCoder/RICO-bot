3faae1143e85ec4b85262f48bff68c0c1b166cfb
Eddie <eddie.mbq@protonmail.com>
2021-03-13 11:37:35 +0000

    2.2.11 - Bug fix to the json file

--- cogs/background.py
+++ cogs/background.py
@@ -4,7 +4,7 @@ from discord.ext import commands, tasks
 import settings
 from itertools import cycle
 
-status = cycle(['Version 2.2.10', 'DM me with complaints!'])
+status = cycle(['Version 2.2.11', 'DM me with complaints!'])
 
 
 class background(commands.Cog):
