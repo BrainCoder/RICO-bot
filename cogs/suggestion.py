b906ebf845d4558219bb35ea597232a57045bb9b
Eddie <eddie.mbq@protonmail.com>
2021-03-17 19:33:18 +0000

    2.2.20 - bug fix to the !suggestion command

--- cogs/suggestion.py
+++ cogs/suggestion.py
@@ -5,17 +5,22 @@ import settings
 
 
 class welcome(commands.Cog):
-
     def __init__(self, client):
         self.client = client
         self._last_member = None
+        self.suggestions = self.client.get_channel(
+            settings.config["channels"]["suggestions"]
+        )
+        self.suggestion_logs = self.client.get_channel(
+            settings.config["channels"]["suggestion-logs"]
+        )
 
     async def send_message(self, channel, suggestion):
         message = await channel.send(suggestion)
         await message.add_reaction("✅")
         await message.add_reaction("❌")
 
-    @commands.command(name="suggest", aliases=['suggestion'])
+    @commands.command(name="suggest", aliases=["suggestion"])
     @commands.cooldown(1, 21600, commands.BucketType.user)
     @commands.has_any_role(
         settings.config["staffRoles"]["admin"],
@@ -24,22 +29,20 @@ class welcome(commands.Cog):
         settings.config["staffRoles"]["semi-moderator"],
         settings.config["staffRoles"]["trial-mod"],
         settings.config["statusRoles"]["vip"],
-        settings.config["statusRoles"]["member"])
+        settings.config["statusRoles"]["member"],
+    )
     async def suggestion(self, ctx, *, suggestion=None):
-        suggestions = self.client.get_channel(settings.config["channels"]["suggestions"])
-        suggestion_logs = self.client.get_channel(settings.config["channels"]["suggestion-logs"])
-        if '@everyone' in suggestion or '@here' in suggestion:
-            return
         if suggestion is None:
             return
-        await self.send_message(suggestions, suggestion)
+        if "@everyone" in suggestion or "@here" in suggestion:
+            return
+        await self.send_message(self.suggestions, suggestion)
         embed = discord.Embed(color=ctx.author.color, timestamp=ctx.message.created_at)
         embed.set_author(name="Suggestion", icon_url=ctx.author.avatar_url)
         embed.add_field(name="Author", value=ctx.author.mention, inline=False)
         embed.add_field(name="Suggestion", value=suggestion, inline=False)
-        await suggestion_logs.send(embed=embed)
+        await self.suggestion_logs.send(embed=embed)
         await ctx.message.delete()
-
 
     @Cog.listener()
     async def on_raw_reaction_add(self, payload):
@@ -68,7 +71,6 @@ class welcome(commands.Cog):
             if yes - nos >= 15:
                 await self.passed_vote(message)
 
-
     async def passed_vote(self, message):
 
         # Remove reactions from message
@@ -80,7 +82,9 @@ class welcome(commands.Cog):
         # Send message to poll board
         channel = self.client.get_channel(settings.config["channels"]["poll-board"])
         mod_ping = message.guild.get_role(settings.config["staffRoles"]["mod-ping"])
-        await self.send_message(channel, f"__**Suggestion**__\n\n```{message.content}```{mod_ping.mention}")
+        await self.send_message(
+            channel, f"__**Suggestion**__\n\n```{message.content}```{mod_ping.mention}"
+        )
 
 
 def setup(client):
