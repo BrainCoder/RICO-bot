eabb0f58af7f4b81ee09e74e490d343670635073
Eddie <eddie.mbq@protonmail.com>
2021-03-19 05:25:58 +0000

    added the define command

--- cogs/misc.py
+++ cogs/misc.py
@@ -9,84 +9,79 @@ from datetime import datetime, timedelta
 import random
 import settings
 import asyncio
+import sys
+import http.client
+import json
 
 from sqlalchemy import text
 
 
 class Extra(commands.Cog):
-
     def __init__(self, client):
         self.client = client
+        self.authkey = sys.argv[4]
 
-    @commands.command(name='selfmute')
-    @commands.cooldown(2, 21600, commands.BucketType.user)
-    @commands.has_any_role(
-        settings.config["staffRoles"]["admin"],
-        settings.config["staffRoles"]["head-moderator"],
-        settings.config["staffRoles"]["moderator"],
-        settings.config["staffRoles"]["semi-moderator"],
-        settings.config["staffRoles"]["trial-mod"],
-        settings.config["statusRoles"]["vip"],
-        settings.config["statusRoles"]["boost-vip"],
-        settings.config["statusRoles"]["member"])
-    async def selfmute(self, ctx):
-        """Lets the user selfmute taking them out of the server"""
-        Selfmute_Role = ctx.guild.get_role(settings.config["statusRoles"]["self-mute"])
-        if Selfmute_Role in ctx.author.roles:
-            await ctx.author.remove_roles(Selfmute_Role)
+    @commands.command(name="urban", aliases=["urb", "define", "def"])
+    # @cooldown(1, 60, commands.BucketType.user)
+    async def urban(self, ctx, *, query):
+        """standard urban dictionary command"""
+
+        if len(query) > 1:
+            nquery = query.replace(" ", "%20")
         else:
-            await ctx.author.add_roles(Selfmute_Role)
-        await ctx.message.delete()
+            nquery = query
+        conn = http.client.HTTPSConnection(
+            "mashape-community-urban-dictionary.p.rapidapi.com"
+        )
+        headers = {
+            "x-rapidapi-key": self.authkey,
+            "x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com",
+        }
 
-    @commands.command(name='rule', aliases=['rules'])
-    @commands.cooldown(1, 5)
-    async def rules(self, ctx, rule: int = None):
-        if rule is None:
-            return
-        try:
-            rules = await utils.extract_data('resources/rules.txt')
-            embed = discord.Embed(color=ctx.author.color, timestamp=ctx.message.created_at)
-            embed.set_author(name="Rules", icon_url=ctx.author.avatar_url)
-            embed.add_field(name=f"Rule {rule}", value=rules[rule - 1])
-            await ctx.send(embed=embed)
-        except IndexError:
-            await ctx.send(f'{rule} is not a valid rule')
+        conn.request("GET", f"/define?term={nquery}", headers=headers)
+        res = conn.getresponse()
+        data = json.loads(res.read().decode("utf-8"))
+        first_def = data["list"][0]
 
-    @commands.command(name="8ball", aliases=['8b'])
+        message = f"**{query}:**\n{first_def['definition']}\n({first_def['permalink']})"
+        await ctx.send(message)
+
+    @commands.command(name="8ball", aliases=["8b"])
     @cooldown(1, 60, commands.BucketType.user)
     async def _8ball(self, ctx, *, question):
         """standard 8ball command"""
         responses = [
-            'It is certain',
-            'It is decidedly so',
-            'Yes Definatley',
-            'You may rely on it',
-            'As I se it, yes',
-            'Most Likely',
-            'Outlook good',
-            'Yes.',
-            'Signs point to yes',
-            'Reply hazy, try again',
-            'Ask again later',
-            'Better not tell you now',
-            'Cannot predict now',
-            'Concentrate and ask again',
-            'Dont count on it',
-            'My reply is no',
-            'My sources say no',
-            'Outlook not so good',
-            'Very doubtful']
-        if '@everyone' in question or '@here' in question:
-            await utils.emoji(ctx, '❌')
+            "It is certain",
+            "It is decidedly so",
+            "Yes Definatley",
+            "You may rely on it",
+            "As I se it, yes",
+            "Most Likely",
+            "Outlook good",
+            "Yes.",
+            "Signs point to yes",
+            "Reply hazy, try again",
+            "Ask again later",
+            "Better not tell you now",
+            "Cannot predict now",
+            "Concentrate and ask again",
+            "Dont count on it",
+            "My reply is no",
+            "My sources say no",
+            "Outlook not so good",
+            "Very doubtful",
+        ]
+        if "@everyone" in question or "@here" in question:
+            await utils.emoji(ctx, "❌")
         else:
-            await ctx.send(f' **Question:** {question}\n**Answer:** {random.choice(responses)}')
-
+            await ctx.send(
+                f" **Question:** {question}\n**Answer:** {random.choice(responses)}"
+            )
 
     @commands.command(name="dosomething")
     async def dosomething(self, ctx):
         """try it and find out ;)"""
         await ctx.channel.send("*Does your mum*")
-
 
     @commands.command(name="userinfo", aliases=["ui"])
     @commands.has_any_role(
@@ -96,7 +91,8 @@ class Extra(commands.Cog):
         settings.config["staffRoles"]["semi-moderator"],
         settings.config["staffRoles"]["trial-mod"],
         settings.config["statusRoles"]["vip"],
-        settings.config["statusRoles"]["boost-vip"])
+        settings.config["statusRoles"]["boost-vip"],
+    )
     async def ui(self, ctx, *, member: discord.Member = None):
         """gives basic info on the user tagged in the arg"""
         if member is None:
@@ -104,19 +100,19 @@ class Extra(commands.Cog):
         member_joined_at = member.joined_at.strftime("%A, %B %d %Y at %H:%M:%S %p")
         result = await database.userdata_select_query(member.id, False)
         if result and result[12] != 0:
-            member_joined_at = (datetime.fromtimestamp(result[12]) -
-                                timedelta(hours=settings.config["memberUpdateInterval"])) \
-                .strftime("%A, %B %d %Y at %H:%M:%S %p")
+            member_joined_at = (
+                datetime.fromtimestamp(result[12])
+                - timedelta(hours=settings.config["memberUpdateInterval"])
+            ).strftime("%A, %B %d %Y at %H:%M:%S %p")
         usernames = await self.build_username_list(member)
         date_created = member.created_at.strftime("%A, %B %d %Y at %H:%M:%S %p")
         user_avatar_url = member.avatar_url
         embed = discord.Embed(color=ctx.author.color, timestamp=ctx.message.created_at)
         embed.set_author(name="UI", icon_url=user_avatar_url)
-        embed.add_field(name='Account was created at: ', value=f"{date_created}.")
+        embed.add_field(name="Account was created at: ", value=f"{date_created}.")
         embed.add_field(name="Member joined at: ", value=f"{member_joined_at}.")
         embed.add_field(name="Previous Usernames: ", value=usernames)
         await ctx.send(embed=embed)
-
 
     @commands.command(name="avatar", aliases=["av"])
     @commands.has_any_role(
@@ -126,26 +122,29 @@ class Extra(commands.Cog):
         settings.config["staffRoles"]["semi-moderator"],
         settings.config["staffRoles"]["trial-mod"],
         settings.config["statusRoles"]["vip"],
-        settings.config["statusRoles"]["boost-vip"])
+        settings.config["statusRoles"]["boost-vip"],
+    )
     async def avatar(self, ctx, *, avamember: discord.Member = None):
         """sends a link of the users avatar"""
         if avamember is None:
             userAvatarUrl = ctx.author.avatar_url
-            await ctx.send(f'<@{ctx.author.id}>s avatar is:\n{userAvatarUrl}')
+            await ctx.send(f"<@{ctx.author.id}>s avatar is:\n{userAvatarUrl}")
         else:
             userAvatarUrl = avamember.avatar_url
             await ctx.send(f"<@{avamember.id}>s avatar is:\n{userAvatarUrl}")
 
-
     @commands.command(name="gfsandwich")
     async def gfsandwich(self, ctx):
         """Evidence that the bot is hounds gf"""
-        hound = await utils.in_roles(ctx.author, settings.config["staffRoles"]["head-dev"])
+        hound = await utils.in_roles(
+            ctx.author, settings.config["staffRoles"]["head-dev"]
+        )
         if not hound:
-            await ctx.send('Ur not my dad :c')
+            await ctx.send("Ur not my dad :c")
         else:
-            await ctx.send('uwu what kinda of sandwich does daddy want =^.^=', delete_after=5)
-
+            await ctx.send(
+                "uwu what kinda of sandwich does daddy want =^.^=", delete_after=5
+            )
 
     @commands.command(name="remind", aliases=["remindme"])
     @commands.has_any_role(
@@ -156,34 +155,35 @@ class Extra(commands.Cog):
         settings.config["staffRoles"]["trial-mod"],
         settings.config["statusRoles"]["vip"],
         settings.config["statusRoles"]["boost-vip"],
-        settings.config["statusRoles"]["member"])
+        settings.config["statusRoles"]["member"],
+    )
     async def remind(self, ctx, *, time: utils.TimeConverter = None):
         """Will ping the user after a specific amount of time in the channel the reminder was sent in."""
         if time is None:
-            await ctx.send('Please specify the timer', delete_after=5)
+            await ctx.send("Please specify the timer", delete_after=5)
         else:
             await utils.emoji(ctx)
             await asyncio.sleep(time)
-            await ctx.send(f'{ctx.author.mention}')
+            await ctx.send(f"{ctx.author.mention}")
 
-
-    @commands.command(name="emergency", aliases=['m'])
+    @commands.command(name="emergency", aliases=["m"])
     async def emergency(self, ctx):
         """Gets an emergency link from emergency.nofap.com website."""
         await utils.get_emergency_picture(ctx)
 
-
     @commands.command(name="tip", aliases=["t"])
     async def tip(self, ctx):
-        tips = await utils.extract_data('resources/tips.txt')
-        await ctx.send(f'{random.choice(tips)}')
-
+        tips = await utils.extract_data("resources/tips.txt")
+        await ctx.send(f"{random.choice(tips)}")
 
     async def build_username_list(self, member):
         with database.engine.connect() as connection:
-            username_query = text("select * from np_db.name_change_event where user_id = " + str(member.id) +
-                                  " and np_db.name_change_event.previous_name not like '[AFK]%' and "
-                                  "np_db.name_change_event.new_name not like '[AFK]%'")
+            username_query = text(
+                "select * from np_db.name_change_event where user_id = "
+                + str(member.id)
+                + " and np_db.name_change_event.previous_name not like '[AFK]%' and "
+                "np_db.name_change_event.new_name not like '[AFK]%'"
+            )
             result = connection.execute(username_query).fetchall()
             if result:
                 # This is used to determine how many usernames are allowed to be in the list.
--- main.py
+++ main.py
@@ -9,11 +9,13 @@ import sys
 import settings
 
 if len(sys.argv) < 5:
-    print("\nArgs entered incorrectly, please refer to the args wikipage:\n"
-        "https://gitlab.com/HellHound0066/noporn-companion/-/wikis/Required-Arguments\n")
+    print(
+        "\nArgs entered incorrectly, please refer to the args wikipage:\n"
+        "https://gitlab.com/HellHound0066/noporn-companion/-/wikis/Required-Arguments\n"
+    )
     sys.exit(0)
 
-with open(sys.argv[1], 'rt') as conf_file:
+with open(sys.argv[1], "rt") as conf_file:
     settings.init()
     settings.config = utils.json.load(conf_file)
     settings.config["databaseUrl"] = sys.argv[2]
@@ -22,45 +24,54 @@ with open(sys.argv[1], 'rt') as conf_file:
 intents = discord.Intents.all()
 intents.members = True
 intents.presences = True
-client = commands.Bot(command_prefix=settings.config["prefix"], intents=intents, case_insensitive=True)
+client = commands.Bot(
+    command_prefix=settings.config["prefix"], intents=intents, case_insensitive=True
+)
+
 
 @client.event
 async def on_ready():
-    print('Bot is active')
+    print("Bot is active")
     await client.wait_until_ready()
     devlogs = client.get_channel(settings.config["channels"]["devlog"])
-    await devlogs.send('---')
-    await devlogs.send(f'{utils.timestr}Bot is online')
-    await devlogs.send(f'{utils.timestr}Loaded `blacklist.txt` & `whitelist.txt` due to startup')
+    await devlogs.send("---")
+    await devlogs.send(f"{utils.timestr}Bot is online")
+    await devlogs.send(
+        f"{utils.timestr}Loaded `blacklist.txt` & `whitelist.txt` due to startup"
+    )
     await cogs_load()
+
 
 async def cogs_load():
     devlogs = client.get_channel(settings.config["channels"]["devlog"])
-    for filename in os.listdir('./cogs'):
-        if filename.endswith('.py'):
-            client.load_extension(f'cogs.{filename[:-3]}')
-            await devlogs.send(f'{utils.timestr}`{filename}` loaded due to startup')
-            print(f'loaded {filename}')
-    await devlogs.send(f'{utils.timestr}**all cogs loaded**')
-    print('all cogs loaded')
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            client.load_extension(f"cogs.{filename[:-3]}")
+            await devlogs.send(f"{utils.timestr}`{filename}` loaded due to startup")
+            print(f"loaded {filename}")
+    await devlogs.send(f"{utils.timestr}**all cogs loaded**")
+    print("all cogs loaded")
+
 
 @client.command(name="logout", aliases=["killswitch"])
-@commands.has_any_role(
-    settings.config["staffRoles"]["admin"])
+@commands.has_any_role(settings.config["staffRoles"]["admin"])
 async def logout(ctx):
     """kills the bot and all its processes"""
     await ctx.send("logging out")
     exit()
 
+
 @client.command(name="creset")
 @commands.has_any_role(
     settings.config["staffRoles"]["head-dev"],
-    settings.config["staffRoles"]["developer"])
+    settings.config["staffRoles"]["developer"],
+)
 async def creset(ctx):
     devlogs = client.get_channel(settings.config["channels"]["devlog"])
-    log = f'{utils.timestr}`cogs` loaded manually using !creset command'
-    client.load_extension('cogs.developer')
+    log = f"{utils.timestr}`cogs` loaded manually using !creset command"
+    client.load_extension("cogs.developer")
     await utils.emoji(ctx)
     await devlogs.send(log)
 
-client.run(sys.argv[4])
+
+client.run(sys.argv[5])
