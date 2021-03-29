c82e16e317147805b9d99cc883e0597e897c1c9b
Eddie <eddie.mbq@protonmail.com>
2021-03-29 03:23:20 +0100

    various bug fixes

--- cogs/logger.py
+++ cogs/logger.py
@@ -10,9 +10,11 @@ import utils
 class logger(commands.Cog):
     def __init__(self, client):
         self.client = client
-        self._last_member = None
         self.del_logs = self.client.get_channel(
             settings.config["channels"]["deleted-logs"]
+        )
+        self.edit_logs = self.client.get_channel(
+            settings.config["channel"]["edited-logs"]
         )
 
     @Cog.listener()
@@ -31,6 +33,24 @@ class logger(commands.Cog):
         )
         embed.add_field(name="Date", value=message.created_at, inline=False)
         await self.del_logs.send(embed=embed)
+
+    @Cog.listener()
+    async def on_message_edit(self, before, after):
+        embed = discord.Embed(
+            color=message.guild.get_role(settings.config["streakRoles"]["150"]),
+            timestamp=datetime.utcnow(),
+            description=f"Message edited in <#{after.channel.id}>",
+        )
+        embed.set_author(name=after.author.name, icon_url=after.author.avatar_url)
+        embed.add_field(name="Before", value=before.content, inline=False)
+        embed.add_field(name="After", value=after.content, inline=False)
+        embed.add_field(
+            name="ID",
+            value=f"```python\nUser = {after.author.id}\nMessage = {after.id}```",
+            inline=False,
+        )
+        embed.add_field(name="Date", value=after.created_at, inline=False)
+        await self.edit_logs.send(embed=embed)
 
 
 def setup(client):
