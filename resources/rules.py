86e776aacba546eba2c3d934f3fc1bb86c4c8e65
Eddie <eddie.mbq@protonmail.com>
2021-03-31 09:18:26 +0100

    2.2.26 - changes to the rules display

---
+++ resources/rules.py
@@ -0,0 +1,34 @@
+rules_dict = {
+    1: [
+        "NSFW/Gore:",
+        "Do not have or post sexually suggestive, potential fetishes or gore containing messages, images, videos, links, profile pictures, statuses, or names. It is up to the staff team's discretion on what is considered too explicit/excessive.",
+    ],
+    2: [
+        "Toxicity/Respect:",
+        "We do not mind friendly banter, but attacking or harassing anyone when they’re clearly uncomfortable (especially when they're seeking help) is unacceptable. Mods will warn verbally once, if the user warned continues said toxic behavior, they will be cooled down, muted, kicked, or banned depending on the severity. It is up to the staff team's discretion on what is considered too excessive.",
+    ],
+    3: [
+        "Spam/Pings:",
+        "Do not spam pings, messages, or images. Do not ghost ping. Do not ping Mods without a good reason.",
+    ],
+    4: [
+        "Advertising/Links:",
+        "DM Advertising without permission, and repeatedly spamming invite links into channels will result in a warning, mute, or ban depending on the severity.",
+    ],
+    5: [
+        "Moderation/Staff:",
+        "Do not disrespect staff members for a moderation decision. Comply and if you really think what happened was unjust then file a complaint by direct messaging the RecoveryHub bot",
+    ],
+    6: [
+        "Server/Community:",
+        "Any action undertaken by a member of this server that can be viewed as hostile/inflammatory to another server is neither encouraged nor allowed.",
+    ],
+    7: [
+        "Misc:",
+        "Do not take the wrong gender role. Do not complain excessively about others.",
+    ],
+    8: [
+        "Discord TOS:",
+        "Do not violate the Discord Terms of Service.\n(https://discordapp.com/terms)",
+    ],
+}
